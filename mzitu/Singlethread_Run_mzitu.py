c83b44b609d294b9bb1d976cebf619fad558e920
zhiqiang <zhiqiang1224@hotmail.com>
2019-08-02 12:49:05 +0800

    HTTP except

--- mzitu/Singlethread_Run_mzitu.py
+++ mzitu/Singlethread_Run_mzitu.py
@@ -18,8 +18,13 @@ headers = {
 
 # 获取页面html
 def GetHtml(url):
-    requests.packages.urllib3.disable_warnings()
-    return requests.get(url, headers=headers,verify=False).text
+    try:
+        requests.packages.urllib3.disable_warnings()
+        return requests.get(url, headers=headers,verify=False).text
+    except:
+        print(url+"异常")
+        return None
+
 
 
 # 定义一个解析妹子总页面的方法，来获取单个妹子的链接
@@ -32,35 +37,36 @@ def parseHtml(html):
 def get_pic(imageUrl):
     print(imageUrl + "正在解析...")
     imageHtml = GetHtml(imageUrl)
+    if imageHtml is not None:
+        soup = BeautifulSoup(imageHtml, "lxml")
+        imgsrc = soup.find("div", class_="main-image").find_all("img")
+        # 标题进行md5加密
+        str = imgsrc[0]["src"]
+        m = hashlib.md5()
+        m.update(str.encode())
+        m.hexdigest()
+        imgtitle = m.hexdigest()
 
-    soup = BeautifulSoup(imageHtml, "lxml")
-    imgsrc = soup.find("div", class_="main-image").find_all("img")
+        # 图片标题 将？转——
+        signpath = soup.find("h2", class_="main-title").get_text().replace('?','_')
+        if "（" in signpath:
+            signpath = signpath[:signpath.index("（")]
 
+        if imgtitle is None:
+            downloadImg(imgsrc[0]["src"])
+        downloadImg(imgsrc[0]["src"], imgtitle, signpath)
+        # time.sleep(1)
 
-    # 标题进行md5加密
-    str = imgsrc[0]["src"]
-    m = hashlib.md5()
-    m.update(str.encode())
-    m.hexdigest()
-    imgtitle = m.hexdigest()
-
-    # 图片标题 将？转——
-    signpath = soup.find("h2", class_="main-title").get_text().replace('?','_')
-    if "（" in signpath:
-        signpath = signpath[:signpath.index("（")]
-
-    if imgtitle is None:
-        downloadImg(imgsrc[0]["src"])
-    downloadImg(imgsrc[0]["src"], imgtitle, signpath)
-    time.sleep(1)
 
 # https://www.mzitu.com/179288
 # 获取页面有多少下一页
 def get_pic_num(listUrl):
     html=GetHtml(listUrl)
-    soup = BeautifulSoup(html, "lxml")
-    list = soup.find("div", class_="pagenavi").find_all("span")
-    return int(list[-2].string)
+    if html is not  None:
+        soup = BeautifulSoup(html, "lxml")
+        list = soup.find("div", class_="pagenavi").find_all("span")
+        return int(list[-2].string)
+    return 0
 
 
 # 下载图片(单线程)
@@ -68,20 +74,21 @@ def downloadImg(imgeurl, name=None, signpath=''):
     print(imgeurl + "正在下载图片...")
     img = requests.get(imgeurl, headers=headers)
     # img = GetHtml(imgeurl)
-    paths = os.getcwd() + os.sep + "image" + os.sep + signpath + os.sep
-    # 判断路径是否存在
-    if not os.path.exists(paths):
-        os.makedirs(paths)
-    if name is None:
-        name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + str(random.randint(10000, 99999)) + ".jpg"
-    else:
-        # name = name + str(random.randint(100, 999)) + ".jpg"
-        name = name + ".jpg"
+    if img is not  None:
+        paths = os.getcwd() + os.sep + "image" + os.sep + signpath + os.sep
+        # 判断路径是否存在
+        if not os.path.exists(paths):
+            os.makedirs(paths)
+        if name is None:
+            name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + str(random.randint(10000, 99999)) + ".jpg"
+        else:
+            # name = name + str(random.randint(100, 999)) + ".jpg"
+            name = name + ".jpg"
 
-    if not os.path.isfile(paths + name):
-        with open(paths + name, 'ab') as f:
-            f.write(img.content)
-            print(paths + name + "下载完成")
+        if not os.path.isfile(paths + name):
+            with open(paths + name, 'ab') as f:
+                f.write(img.content)
+                print(paths + name + "下载完成")
 
 
 
