cb706c6ae5b91470400d293156fb78fff25af0c5
zhiqiang <zhiqiang122@hotmail.com>
2019-08-07 20:49:11 +0800

    del

--- Reptile/jdcomment/jdcomment.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome()
-if __name__ =='__main__':
-    browser = webdriver.Chrome()
-    browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
-    input.send_keys('Python')
-
